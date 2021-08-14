9705024ee4bebbc02642fcbc90cb4cb5af98422c
Crowdbotics <team@crowdbotics.com>
2021-08-14 17:21:04 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "bandanauppup_29809Identifier",
+  url: "https://bandanauppup_29809Identifier.botics.co",
+  api: "https://bandanauppup_29809Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
